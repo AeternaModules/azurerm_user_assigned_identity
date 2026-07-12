57eb56a93e61e022c845fd6e518c90e8fe499d54
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "user_assigned_identities" {
-  description = "All user_assigned_identity resources"
-  value       = azurerm_user_assigned_identity.user_assigned_identities
-}
 output "user_assigned_identities_client_id" {
-  description = "List of client_id values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.client_id]
+  description = "Map of client_id values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.client_id }
 }
 output "user_assigned_identities_isolation_scope" {
-  description = "List of isolation_scope values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.isolation_scope]
+  description = "Map of isolation_scope values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.isolation_scope }
 }
 output "user_assigned_identities_location" {
-  description = "List of location values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.location]
+  description = "Map of location values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.location }
 }
 output "user_assigned_identities_name" {
-  description = "List of name values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.name]
+  description = "Map of name values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.name }
 }
 output "user_assigned_identities_principal_id" {
-  description = "List of principal_id values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.principal_id]
+  description = "Map of principal_id values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.principal_id }
 }
 output "user_assigned_identities_resource_group_name" {
-  description = "List of resource_group_name values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.resource_group_name]
+  description = "Map of resource_group_name values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.resource_group_name }
 }
 output "user_assigned_identities_tags" {
-  description = "List of tags values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.tags]
+  description = "Map of tags values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.tags }
 }
 output "user_assigned_identities_tenant_id" {
-  description = "List of tenant_id values across all user_assigned_identities"
-  value       = [for k, v in azurerm_user_assigned_identity.user_assigned_identities : v.tenant_id]
+  description = "Map of tenant_id values across all user_assigned_identities, keyed the same as var.user_assigned_identities"
+  value       = { for k, v in azurerm_user_assigned_identity.user_assigned_identities : k => v.tenant_id }
 }
 
